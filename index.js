4bf51789202813e95488a5d86f54e5edb30a6a6d
HarryPreet <harrypreet1999@gmail.com>
2021-12-17 16:37:32 -0800

    changed file loc

---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+const { Pool } = require('pg');
+const { response } = require('express');
+
+var pool;
+
+pool = new Pool({
+  connectionString: 'postgres://postgres:&UJM,ki8@localhost/users'
+})
+
+var app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/index'))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+
+
+app.get('/database', (req, res) => {
+  var getUsersQuery = 'SELECT * FROM person';
+  pool.query(getUsersQuery, (error, result) => {
+    if (error)
+      res.end(error)
+    var results = { 'rows': result.rows }
+    res.render('pages/db', results)
+  })
+})
+
+app.post('/addperson', (req, res) => {
+  var pname = req.body.name;
+  var size = req.body.size;
+  var height = req.body.height;
+  var type = req.body.type;
+  var insertUsersQuery = `INSERT INTO person values('${pname}',${size},${height},'${type}')`;
+ 
+  pool.query(insertUsersQuery, (error, result) => {
+    if (error)
+      res.end(error)
+    
+  })
+  var status = 'created';
+  var getUsersQuery = 'SELECT * FROM person';
+  pool.query(getUsersQuery, (error, result) => {
+    if (error)
+      res.end(error)
+    var results = { 'rows': result.rows }
+    res.render('pages/db', results)
+  })
+}) 
+
+app.post('/deleteperson', (req, res) => {
+  var pname = req.body.name;
+  var insertUsersQuery = `delete from person where person.name= '${pname}'`;
+  pool.query(insertUsersQuery, (error, result) => {
+    if (error)
+      res.end(error)  
+  })
+  var status = 'deleted';
+  var getUsersQuery = 'SELECT * FROM person';
+  pool.query(getUsersQuery, (error, result) => {
+    if (error)
+      res.end(error)
+    var results = { 'rows': result.rows }
+    res.render('pages/db', results)
+  })
+}) 
+
+app.post('/editperson', (req, res) => {
+  var pname = req.body.name;
+  var size = req.body.size;
+  var height = req.body.height;
+  var type = req.body.type;
+  var insertUsersQuery = `update person set size =${size}, height = ${height}, type ='${type}' where name= '${pname}'`;
+  console.log(insertUsersQuery);
+  pool.query(insertUsersQuery, (error, result) => {
+    if (error)
+      res.end(error)  
+  })
+  var status = 'updated';
+  var getUsersQuery = 'SELECT * FROM person';
+  pool.query(getUsersQuery, (error, result) => {
+    if (error)
+      res.end(error)
+    var results = { 'rows': result.rows }
+    res.render('pages/db', results)
+  })
+}) 
